010ca5ba987d650efeab72ba85c7b957e0bd9630
Toni Daada <tonidaadaadagmail.com>
2024-12-16 11:55:08 +0100

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 'use strict';
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const section1 = document.getElementById('section--1');
+const header = document.querySelector('.header');
 
 // Buttons
 const btnShowModal = document.querySelectorAll('.btn--show-modal');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
 // Functions
-const showModal = function () {
+const showModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 const closeModal = function () {
   modal.classList.add('hidden');
-  overlay.classList.toggle('hidden');
+  overlay.classList.add('hidden');
 };
 
 // Event listeners for the modal button
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', showModal);
-}
+btnShowModal.forEach(value => {
+  value.addEventListener('click', showModal);
+});
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', e => {
-  console.log(e.key);
   e.key === 'Escape' ? closeModal() : '';
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // Normal way of scrolling
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // Modern Scrolling, only works in modern browsers
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Smooth Scrolling Page Navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
